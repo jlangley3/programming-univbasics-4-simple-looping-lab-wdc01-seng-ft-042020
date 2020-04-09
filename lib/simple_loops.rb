751c837a124c8ac7cfcbd11db3417e03f4cbba29
Jesse Langley <jlangleyiii@aol.com>
2020-04-09 15:33:08 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,9 @@
-# Write your methods here
+def loop_message_five_times    # Write your methods here
+counter = 0
+while counter < 5 do
+  puts "This is my message 5 times"
+ end
+ counter += 1
+end
+
+loop_message_five_times
